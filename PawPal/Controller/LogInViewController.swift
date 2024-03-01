db0aee21454c0666ed80514cd1ae3319a737a80b
Happyvolt92 <elodiegage@gmail.com>
2024-03-01 20:44:34 +0100

    start Firebase added : View Launchscreen, HomeScreen, started empty controller log/signin view, assets, Icon,

---
+++ PawPal/Controller/LogInViewController.swift
@@ -0,0 +1,8 @@
+//
+//  LoginViewController.swift
+//  PawPal
+//
+//  Created by HappyVolt on 01/03/2024.
+//
+
+import Foundation
---
+++ PawPal/Controller/SignInViewController.swift
@@ -0,0 +1,8 @@
+//
+//  SignInViewController.swift
+//  PawPal
+//
+//  Created by HappyVolt on 01/03/2024.
+//
+
+import Foundation
