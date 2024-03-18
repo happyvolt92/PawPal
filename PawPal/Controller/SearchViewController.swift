24e13b3ed151ccbb213d2e6da8c4224081c506f7
Happyvolt92 <elodiegage@gmail.com>
2024-03-18 16:21:02 +0100

    SearchView UI started

---
+++ PawPal/Controller/SearchViewController.swift
@@ -0,0 +1,13 @@
+//
+//  SearchViewController.swift
+//  PawPal
+//
+//  Created by HappyVolt on 18/03/2024.
+//
+
+import Foundation
+import UIKit
+
+class SearchViewController: UIViewController {
+    
+}
